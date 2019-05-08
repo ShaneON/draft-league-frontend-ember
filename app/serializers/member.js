2a843c4dfa0eaf218db27ca35b8b85f520b08c8c
Shane <sgoneill89@gmail.com>
2019-05-08 20:03:15 +0100

    set up mirage

--- app/serializers/member.js
+++ app/serializers/member.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
   normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
-  const newPayload = payload.map(t => this._toNormalisedPayload(t));
-  return this._super(store, primaryModelClass, newPayload, id, requestType);
+    const newPayload = payload.map(t => this._toNormalisedPayload(t));
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
   },
 
   normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType) {
